0ac95a322c3369f1ff464bdeb071724417bf87c2
Philip Chaplin <42811531+guitarc123@users.noreply.github.com>
2025-01-17 11:28:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+function clock() {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let ampm = "PM";
+
+    let time = document.querySelector(".time");
+    let dateElement = document.querySelector(".date");
+
+    if (h < 12) {
+        ampm = "AM";
+    }
+
+    if (h === 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+    }
+
+    if (h < 10) {
+        h = "0" + h;
+    }
+
+    if (m < 10) {
+        m = "0" + m;
+    }
+
+    if (s < 10) {
+        s = "0" + s;
+    }
+
+    time.innerHTML = h + ":" + m + ":" + s + " " + ampm;
+
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    dateElement.innerHTML =
+        d.getDate() +
+        " " +
+        months[d.getMonth()] +
+        " " +
+        d.getFullYear() +
+        ", " +
+        days[d.getDay()];
+}
+
+setInterval(clock, 1000);
